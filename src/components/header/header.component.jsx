5cb474b0e762d5a0aa9a372fba170de1d4861983
Uktu94 <utkukaraaslan94@gmail.com>
2020-04-17 22:36:49 +0300

    added cartIcon and cart-dropdown components to app, also implemented card reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,39 +3,46 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/original.svg'
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
         </Link>
         <div className='options'>
-        <Link className='option' to='/shop'>
-            SHOP
+            <Link className='option' to='/shop'>
+                SHOP
         </Link>
-        <Link className='option' to='/shop'>
-            CONTACT
+            <Link className='option' to='/shop'>
+                CONTACT
         </Link>
-        {
-            currentUser ?
-            <div 
-            className='option' 
-            onClick={() => auth.signOut()}>
-            SIGN OUT
+            {
+                currentUser ?
+                    <div
+                        className='option'
+                        onClick={() => auth.signOut()}>
+                        SIGN OUT
             </div>
-            :
-            <Link className='option' to='/signin'>SIGN IN</Link>
-        }
+                    :
+                    <Link className='option' to='/signin'>SIGN IN</Link>
+            }
+            <CartIcon />
         </div>
+        {
+            hidden ? null : <CartDropdown />
+        }
     </div>
 );
 
-const mapStateToTrops = state => ({
-    currentUser: state.user.currentUser
+const mapStateToTrops = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToTrops)(Header);
