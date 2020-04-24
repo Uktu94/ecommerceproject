9be20c15726176a59ac1e1ab9cb5afbb1d334455
Uktu94 <utkukaraaslan94@gmail.com>
2020-04-25 02:58:35 +0300

    shop undefined fixed

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,6 +1,6 @@
 import { createSelector } from 'reselect';
 
-const selectShop = state => state.selectShop
+const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
     [selectShop],
