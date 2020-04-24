a0220df3656d23ea737fe1978d0885a25704da31
Uktu94 <utkukaraaslan94@gmail.com>
2020-04-24 20:38:04 +0300

    Shop is undefined issue

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,33 +1,13 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data';
-
-import CollectionPreview from '../../components/collection-preview/collection-preview';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props)
+const ShopPage = ({ collections }) => (
+    <div className='shop-page'>
+        <CollectionsOverview />
+    </div>
+);
 
-        this.state = {
-            collections: SHOP_DATA
-
-        }
-    }
-
-    render() {
-        const { collections } = this.state;
-        return (<div className='shop-page'>
-            {
-                collections.map(({ id, ...oterCollectionProps }) => (
-                    <CollectionPreview key={id} {...oterCollectionProps} />
-                ))
-            }
-
-        </div>)
-
-    }
-
-}
 
 export default ShopPage;
