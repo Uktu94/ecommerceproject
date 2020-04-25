e46a8392f15c2890958a996197de9eef974029ff
Uktu94 <utkukaraaslan94@gmail.com>
2020-04-25 22:23:30 +0300

    updating stripe integration and updating favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,8 +3,12 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
-import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
+import {
+    selectCartItems,
+    selectCartTotal
+} from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
 
@@ -39,6 +43,14 @@ const CheckoutPage = ({ cartItems, total }) => (
         <div className='total'>
             <span>TOTAL ${total}</span>
         </div>
+        <div className='test-warning'>
+           *Please use the following test credit card for payments*
+           <br />
+           *Ödemeler için lütfen aşağıdaki kredi kartını kullanınız*
+           <br />
+           4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
+        </div>
+        <StripeCheckoutButton price={total} />
     </div>
 )
 
